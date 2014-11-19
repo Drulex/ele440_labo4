79769cae3aa965774cec3cbb18f7848291c37085
Alexandru Jora <alexandru@jora.ca>
2014-11-18 21:15:52 -0500

    added functions to calculate conflicts and fitness of solution

--- n-queens.py
+++ n-queens.py
@@ -27,6 +27,7 @@ class Board:
 		self.board[x][y] = 'Q'
 
 
+
 def build_board_from_solution(N, sol):
 	'''
 	Function to build board from solution array
@@ -36,7 +37,7 @@ def build_board_from_solution(N, sol):
 
 	#  We add the queens on the board
 	for i in xrange(len(sol)):
-		board.add_queen(i, sol[i])
+		board.add_queen(sol[i], i)
 
 	#  We print the board
 	board.print_board()
@@ -64,9 +65,46 @@ def parse_input_data(infile):
 		return N, sol_array
 
 
+def calc_conflict(sol, xpos, ypos):
+	'''
+	Function to calculate number of conflicts AT THE RIGHT SIDE
+	of a given queen. This cannot be used to calculate all conflicts
+	of a queen, but it is used when iteraing queens from left to right
+	on a given board
+	'''
+	conflict_count = 0
+	k = 1
+	for j in xrange(xpos + 1, len(sol)):
+		if sol[j] == ypos + k:
+			print
+			conflict_count += 1
+		if sol[j] == ypos - k:
+			conflict_count += 1
+		if sol[j] == ypos:
+			conflict_count += 1
+		k += 1
+	return conflict_count
+
+
+def calc_fitness(sol):
+	'''
+	Function to calculate fitness function of a solution
+	'''
+	conflicts = 0
+
+	#  for every queen starting from the first column (most left)
+	#  we calculate the number of conflicts and increment
+	for i in xrange(0, len(sol)):
+		conflicts += calc_conflict(sol, i, sol[i])
+	return conflicts
+
 
 if __name__ == '__main__':
 	infile = 'fichierTest-20-8.txt'
-	N, solutions = parse_input_data(infile)
-	for i in xrange(len(solutions)):
-		build_board_from_solution(N, solutions[i])
+	#N, solutions = parse_input_data(infile)
+	#build_board_from_solution(N, solutions[0])
+	#print solutions[0]
+	sol = [1, 3, 4, 2, 1, 7, 5, 0]
+	sol2 = [6, 4, 2, 0, 5, 7, 1, 3]
+	build_board_from_solution(8, sol2)
+	print 'fitness: ', calc_fitness(sol2)
