e0f7441dcfa95e263d854f356e9268429bd99300
Alexandru Jora <alexandru@jora.ca>
2014-11-20 08:56:36 -0500

    move most functions inside Board class. Will make it easier to manage

--- n-queens.py
+++ n-queens.py
@@ -3,11 +3,14 @@ class Board:
 	Board class represents the chessboard and methods available to manipulate it.
 	'''
 
-	def __init__(self, N):
+	def __init__(self, N, sol):
 		'''
 		We create a 2d array (list of lists) of size N x N
+		We create an array to hold the solution in proper encoding format
 		'''
 		self.board = [[' ' for x in range(N)] for y in range(N)]
+		self.sol = sol
+		self.populate_board(N)
 
 
 	def print_board(self):
@@ -27,21 +30,55 @@ class Board:
 		self.board[x][y] = 'Q'
 
 
+	def populate_board(self, N):
+		'''
+		Function to populate board based on sol_array
+		'''
+		#  We add the queens on the board
+		for i in xrange(N):
+			self.add_queen(self.sol[i], i)
 
-def build_board_from_solution(N, sol):
-	'''
-	Function to build board from solution array
-	'''
-	#  We create board object
-	board = Board(N)
 
-	#  We add the queens on the board
-	for i in xrange(len(sol)):
-		board.add_queen(sol[i], i)
+	def calc_conflict(self, xpos, ypos):
+		'''
+		Function to calculate number of conflicts AT THE RIGHT SIDE
+		of a given queen. This cannot be used to calculate all conflicts
+		of a queen, but it is used when iteraing queens from left to right
+		on a given board
+		'''
+		conflict_count = 0
+		k = 1
+		for j in xrange(xpos + 1, len(self.sol)):
+			if self.sol[j] == ypos + k:
+				conflict_count += 1
+			if self.sol[j] == ypos - k:
+				conflict_count += 1
+			if self.sol[j] == ypos:
+				conflict_count += 1
+			k += 1
+		return conflict_count
 
-	#  We print the board
-	board.print_board()
-	return board
+
+	def calc_fitness(self):
+		'''
+		Function to calculate fitness function of a solution
+		'''
+		conflicts = 0
+
+		#  for every queen starting from the first column (most left)
+		#  we calculate the number of conflicts and increment it
+		for i in xrange(0, len(self.sol)):
+			conflicts += self.calc_conflict(i, self.sol[i])
+		return -conflicts
+
+
+	def check_if_valid(self):
+		'''
+		Check if current chessboard is a valid solution to the problem
+		'''
+		pass
+
+
 
 
 def parse_input_data(infile):
@@ -65,42 +102,19 @@ def parse_input_data(infile):
 		return N, sol_array
 
 
-def calc_conflict(sol, xpos, ypos):
-	'''
-	Function to calculate number of conflicts AT THE RIGHT SIDE
-	of a given queen. This cannot be used to calculate all conflicts
-	of a queen, but it is used when iteraing queens from left to right
-	on a given board
-	'''
-	conflict_count = 0
-	k = 1
-	for j in xrange(xpos + 1, len(sol)):
-		if sol[j] == ypos + k:
-			conflict_count += 1
-		if sol[j] == ypos - k:
-			conflict_count += 1
-		if sol[j] == ypos:
-			conflict_count += 1
-		k += 1
-	return conflict_count
 
 
-def calc_fitness(sol):
-	'''
-	Function to calculate fitness function of a solution
-	'''
-	conflicts = 0
 
-	#  for every queen starting from the first column (most left)
-	#  we calculate the number of conflicts and increment it
-	for i in xrange(0, len(sol)):
-		conflicts += calc_conflict(sol, i, sol[i])
-	return -conflicts
+
 
 
 if __name__ == '__main__':
 	infile = 'fichierTest-20-8.txt'
 	N, solutions = parse_input_data(infile)
+	b_sol = []
 	for s in solutions:
-		build_board_from_solution(N, s)
-		print 'fitness =', calc_fitness(s)
+		b_sol.append(Board(N, s))
+	for board in b_sol:
+		board.print_board()
+		print board.calc_fitness()
+
