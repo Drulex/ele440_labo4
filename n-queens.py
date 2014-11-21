9aeafd1f86e796406e516b63f94668154e162404
Alexandru Jora <alexandru@jora.ca>
2014-11-20 21:35:08 -0500

    add recombine and two point crossover functions. small formatting fixes

--- n-queens.py
+++ n-queens.py
@@ -1,127 +1,165 @@
+from random import randint, sample
+import sys
+
 class Board:
-	'''
-	Board class represents the chessboard and methods available to manipulate it.
-	'''
+    '''
+    Board class represents the chessboard and methods available to manipulate it.
+    '''
 
-	def __init__(self, N, sol):
-		'''
-		We create a 2d array (list of lists) of size N x N
-		We create an array to hold the solution in proper encoding format
-		'''
-		self.board = [[' ' for x in range(N)] for y in range(N)]
-		self.sol = sol
-		self.populate_board(N)
+    def __init__(self, N, sol):
+        '''
+        We create a 2d array (list of lists) of size N x N
+        We create an array to hold the solution in proper encoding format
+        '''
+        self.board = [[' ' for x in range(N)] for y in range(N)]
+        self.sol = sol
+        self.populate_board(N)
 
+    def print_board(self):
+        '''
+        Print the current board
+        '''
+        print '----------------------------------------'
+        for row in self.board:
+            print row
+        print '----------------------------------------'
 
-	def print_board(self):
-		'''
-		Print the current board
-		'''
-		print '----------------------------------------'
-		for row in self.board:
-			print row
-		print '----------------------------------------'
+    def add_queen(self, x, y):
+        '''
+        Add a queen on the board at position x, y
+        '''
+        self.board[x][y] = 'Q'
 
+    def populate_board(self, N):
+        '''
+        Function to populate board based on sol_array
+        '''
+        #  We add the queens on the board
+        for i in xrange(N):
+            self.add_queen(self.sol[i], i)
 
-	def add_queen(self, x, y):
-		'''
-		Add a queen on the board at position x, y
-		'''
-		self.board[x][y] = 'Q'
+    def calc_conflict(self, xpos, ypos):
+        '''
+        Function to calculate number of conflicts AT THE RIGHT SIDE
+        of a given queen. This cannot be used to calculate all conflicts
+        of a queen, but it is used when iterating queens from left to right
+        on a given board
+        '''
+        conflict_count = 0
+        k = 1
+        for j in xrange(xpos + 1, len(self.sol)):
+            if self.sol[j] == ypos + k:
+                conflict_count += 1
+            if self.sol[j] == ypos - k:
+                conflict_count += 1
+            if self.sol[j] == ypos:
+                conflict_count += 1
+            k += 1
+        return conflict_count
 
+    def calc_fitness(self):
+        '''
+        Function to calculate fitness function of a solution
+        '''
+        conflicts = 0
 
-	def populate_board(self, N):
-		'''
-		Function to populate board based on sol_array
-		'''
-		#  We add the queens on the board
-		for i in xrange(N):
-			self.add_queen(self.sol[i], i)
+        #  for every queen starting from the first column (most left)
+        #  we calculate the number of conflicts and increment it
+        for i in xrange(0, len(self.sol)):
+            conflicts += self.calc_conflict(i, self.sol[i])
+        return -conflicts
 
-
-	def calc_conflict(self, xpos, ypos):
-		'''
-		Function to calculate number of conflicts AT THE RIGHT SIDE
-		of a given queen. This cannot be used to calculate all conflicts
-		of a queen, but it is used when iterating queens from left to right
-		on a given board
-		'''
-		conflict_count = 0
-		k = 1
-		for j in xrange(xpos + 1, len(self.sol)):
-			if self.sol[j] == ypos + k:
-				conflict_count += 1
-			if self.sol[j] == ypos - k:
-				conflict_count += 1
-			if self.sol[j] == ypos:
-				conflict_count += 1
-			k += 1
-		return conflict_count
-
-
-	def calc_fitness(self):
-		'''
-		Function to calculate fitness function of a solution
-		'''
-		conflicts = 0
-
-		#  for every queen starting from the first column (most left)
-		#  we calculate the number of conflicts and increment it
-		for i in xrange(0, len(self.sol)):
-			conflicts += self.calc_conflict(i, self.sol[i])
-		return -conflicts
-
-
-	def check_if_optimal(self):
-		'''
-		Check if current chessboard is a optimal solution to the problem
-		'''
-		if self.calc_fitness == 0:
-			return True
-		else:
-			return False
+    def check_if_optimal(self):
+        '''
+        Check if current chessboard is a optimal solution to the problem
+        '''
+        if self.calc_fitness == 0:
+            return True
+        else:
+            return False
 
 
 class Population:
-	'''
-	Class used to manipulation population
-	'''
+    '''
+    Class used to manipulation population object
+    '''
 
-	def __init__(self, N, solutions):
-		self.population = []
-		for s in solutions:
-			self.population.append(Board(N, s))
+    def __init__(self, N, solutions):
+        self.population = []
+        self.solutions = solutions
+        self.create_population(N)
+
+    def create_population(self, N):
+        '''
+        Take each solution and create board
+        '''
+        for s in self.solutions:
+            self.population.append(Board(N, s))
+
+    def two_points_crossover(self, sol1, sol2):
+        '''
+        Function to execute 2 points crossover on 2 chromosomes
+        '''
+        size = min(len(sol1), len(sol2))
+        pt1 = randint(1, size)
+        pt2 = randint(1, size - 1)
+        if pt2 >= pt1:
+            pt2 += 1
+        else: # Swap the two cx points
+            pt1, pt2 = pt2, pt1
+
+        sol1[pt1:pt2], sol2[pt1:pt2] = sol2[pt1:pt2], sol1[pt1:pt2]
+
+    def recombine(self):
+        '''
+        Recombination algorithm. Selects 2 solutions at random and recombines them
+        We may choose the crossover function we want
+        '''
+        #  we select 2 solutions at random
+        randi = sample(xrange(len(self.population)), 2)
+
+        #  we recombine them using 2 points crossover
+        self.two_points_crossover(self.solutions[randi[0]], self.solutions[randi[1]])
+
+
 
 
 def parse_input_data(infile):
-	'''
-	Function parse data from input file
-	Saves the solutions in an array of arrays called sol_array
-	Returns N and sol_array
-	'''
-	with open(infile, 'r') as f:
-		nsol = int(f.readline())
-		N = int(f.readline())
-		sol_array = []
-		for line in f.readlines():
+    '''
+    Function parse data from input file
+    Saves the solutions in an array of arrays called sol_array
+    Returns N and sol_array
+    '''
+    with open(infile, 'r') as f:
+        nsol = int(f.readline())
+        N = int(f.readline())
+        sol_array = []
+        for line in f.readlines():
 
-			#  For each line we split and convert values to int before adding to array
-			sol = [int(x) for x in line.split()]
+            #  For each line we split and convert values to int before adding to array
+            sol = [int(x) for x in line.split()]
 
-			#  If array is not empty we append it to array of solutions
-			if sol:
-				sol_array.append(sol)
-		return N, sol_array
+            #  If array is not empty we append it to array of solutions
+            if sol:
+                sol_array.append(sol)
+        return N, sol_array
 
+
+def check_for_optimal(pop):
+    for b in pop.population:
+        print b.calc_fitness()
+        if b.check_if_optimal() is True:
+            print "FOUND OPTIMAL SOLUTION"
+            sys.exit(1)
 
 
 if __name__ == '__main__':
-	infile = 'fichierTest-20-8.txt'
-	N, solutions = parse_input_data(infile)
-	Pop = Population(N, solutions)
-
-	for board in Pop.population:
-		board.print_board()
-		print board.calc_fitness()
-		print board.check_if_optimal()
-
+    infile = 'fichierTest-20-8.txt'
+    N, solutions = parse_input_data(infile)
+    Pop = Population(N, solutions)
+    iterations = 0
+    while iterations < 10000:
+        print 'iteration=', iterations
+        check_for_optimal(Pop)
+        Pop.recombine()
+        iterations += 1
