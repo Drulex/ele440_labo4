8c36f11bbf9f2edd199abd8d62e1b3120278f577
Alexandru Jora <alexandru@jora.ca>
2014-12-02 08:23:23 -0500

    remove function to graph average fitness

--- n-queens.py
+++ n-queens.py
@@ -47,19 +47,6 @@ def parse_input_data(infile):
         return N, sol_array
 
 
-def graph_fitness_over_time(data):
-    '''
-    Print evolution of average fitness over time
-    '''
-    fig = plt.figure()
-    axes = fig.add_axes([0.1, 0.1, 0.8, 0.8])
-    axes.plot(data.keys(), data.values(), 'r')
-    axes.set_xlabel('Generation')
-    axes.set_ylabel('Average fitness')
-    axes.set_title('Evolution of average fitness over time')
-    plt.show()
-
-
 def generate_population(N, pop_size):
     '''
     Generates a random population based on user input
